fcefe474c1940f0081323c6554e1f8f34ce0ae5e
Stephen Tredger <stredger@gmail.com>
2018-05-07 14:46:54 -0700

    add wrapper for bcgov bootstrap

---
+++ src/bcgov_bootstrap/jquery_wrapper.js
@@ -0,0 +1,5 @@
+import jquery from 'jquery'
+
+// make jquery globally avaliable
+global.jQuery = jquery
+global.$ = jquery
---
+++ src/bcgov_bootstrap/styles.js
@@ -0,0 +1,1 @@
+import 'bootstrap-theme/dist/css/bootstrap-theme.min.css'
