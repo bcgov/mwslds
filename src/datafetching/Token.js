6293f06240c584b66a7b1660e5592de79c113a67
Stephen Tredger <stredger@gmail.com>
2018-05-17 15:41:10 -0700

    add function to load oauth token
    doesnt work with the ministry oauth site though as the preflight OPTIONS
    request require auth, but the browser doesnt send the authorization header
    when doing preflight requests...

--- src/datafetching/Token.js
+++ src/datafetching/Token.js
@@ -1,13 +1,46 @@
 import React from 'react'
+import base64 from 'base-64'
 
 import currToken from './Secret'
 
-const token = currToken
+const tokenRoute = 'https://i1api.nrs.gov.bc.ca/oauth2/v1/oauth/token?disableDeveloperFilter=true&grant_type=client_credentials&scope=MWSL_COMMONMINES_API.*'
+
+let token = currToken
+
+export function loadToken(username, password) {
+  const auth = base64.encode(`${username}:${password}`)
+
+  const options = {
+    headers: new Headers({
+      Authorization: `Basic ${auth}`,
+    }),
+    mode: 'cors',
+  }
+
+  fetch(tokenRoute, options)
+    .then((resp) => {
+      if (!resp.ok) {
+        throw Error(resp.statusText)
+      }
+      return resp.json()
+    })
+    .then((parsed) => {
+      token = parsed.access_token
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
 
 export default function withToken(Wrapped) {
   return class WithTokenHOC extends React.Component {
     constructor(props) {
       super(props)
+
+      if (!token) {
+        loadToken()
+      }
+
       this.state = {
         token,
       }
