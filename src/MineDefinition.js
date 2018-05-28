5572fa8505f02fb7787e966245f467641e08410c
Stephen Tredger <stredger@gmail.com>
2018-05-28 10:11:08 -0700

    make row expand display use inputs

--- src/MineDefinition.js
+++ src/MineDefinition.js
@@ -3,7 +3,95 @@ import { selectTransform } from './datatransform'
 
 
 const detailFields = [
-
+  {
+    name: 'mineName',
+    validator: isRequired,
+    inputGroup: 0,
+    width: 100,
+  },
+  {
+    name: 'alias',
+    inputGroup: 1,
+    width: 100,
+  },
+  {
+    name: 'mineLocationName',
+    inputGroup: 2,
+    width: 100,
+  },
+  {
+    name: 'district',
+    inputGroup: 3,
+    width: 100,
+  },
+  {
+    name: 'mineManager',
+    inputGroup: 4,
+    width: 100,
+  },
+  {
+    name: 'enteredBy',
+    inputGroup: 5,
+    width: 50,
+    disabled: true,
+  },
+  {
+    name: 'enteredDate',
+    inputGroup: 5,
+    width: 50,
+    disabled: true,
+  },
+  // {
+  //   name: 'permitteeCompanyCode',
+  //   type: 'data-select',
+  //   route: 'companies',
+  //   validator: isRequired,
+  //   transform: selectTransform('companies', 'code', 'code'),
+  //   inputGroup: 7,
+  //   width: 25,
+  // },
+  {
+    name: 'regionCode',
+    type: 'data-select',
+    route: 'regions',
+    validator: isRequired,
+    transform: selectTransform('regions', 'code', 'code'),
+    inputGroup: 7,
+    width: 25,
+  },
+  {
+    name: 'mineTypeCode',
+    type: 'data-select',
+    route: 'minetypes',
+    validator: isRequired,
+    transform: selectTransform('mineTypes', 'code', 'name'),
+    inputGroup: 7,
+    width: 25,
+  },
+  {
+    name: 'mineStatusCode',
+    type: 'data-select',
+    route: 'minestatuses',
+    validator: isRequired,
+    transform: selectTransform('mineStatuses', 'code', 'name'),
+    inputGroup: 7,
+    width: 25,
+  },
+  {
+    name: 'major',
+    type: 'checkbox',
+    inputGroup: 8,
+  },
+  {
+    name: 'underInvestigation',
+    type: 'checkbox',
+    inputGroup: 8,
+  },
+  {
+    name: 'withIssues',
+    type: 'checkbox',
+    inputGroup: 8,
+  },
 ]
 export { detailFields }
 
