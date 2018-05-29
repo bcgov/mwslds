12dfc1d9823c6d949c391ca03eafe07ecf5e054b
Stephen Tredger <stredger@gmail.com>
2018-05-29 09:04:20 -0700

    update token url

--- src/datafetching/Routes.js
+++ src/datafetching/Routes.js
@@ -1,7 +1,7 @@
 
 const BASE_URL = 'https://i1api.nrs.gov.bc.ca/mwsl-commonmines-api/v1'
 const MINES_ROUTE = 'mines'
-const TOKEN_URL = 'https://i1api.nrs.gov.bc.ca/oauth2/v1/oauth/token?disableDeveloperFilter=true&grant_type=client_credentials&scope=MWSL_COMMONMINES_API.*'
+const TOKEN_URL = 'https://i1api.nrs.gov.bc.ca/oauth2/v1/oauth/token?disableDeveloperFilter=true&grant_type=client_credentials&scope=MWSL_COMMONMINES_API.*,MWSL_REPORTS_API.*'
 
 export { BASE_URL }
 export { MINES_ROUTE }
