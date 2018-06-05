6fb4c998098e38a1627707ac97590bdd084a2fb5
Stephen Tredger <stredger@gmail.com>
2018-06-05 14:22:32 -0700

    add index.js for datafetching

---
+++ src/datafetching/index.js
@@ -0,0 +1,13 @@
+import withData from './DataLoader'
+import withToken from './Token'
+import Routes from './Routes'
+
+export { withData }
+export { withToken }
+export { Routes }
+
+export default {
+  withData,
+  withToken,
+  Routes,
+}
