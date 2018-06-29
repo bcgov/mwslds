ffdc810b9d176649cc7d556ba776163e12e3d3dd
Stephen Tredger <stredger@gmail.com>
2018-06-29 11:51:19 -0700

    upgrade bootstrap and jquery dependencies

--- src/style/actions.js
+++ src/style/actions.js
@@ -3,4 +3,4 @@
 import './jquery_wrapper'
 
 // this guy requires jQuery to be globally avaliable so we import the wrapper before
-import 'Gov-2.0-Bootstrap-Skeleton/dist/bcgov-bootstrap'
+import 'Gov-2.0-Bootstrap-Skeleton/dist/js/bcgov-bootstrap'
