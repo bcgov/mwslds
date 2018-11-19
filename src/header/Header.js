5acf7463f13d1482627662cff30e80496a102def
Stephen Tredger <stredger@gmail.com>
2018-11-18 19:52:51 -0800

    fix table and header heights

--- src/header/Header.js
+++ src/header/Header.js
@@ -26,11 +26,11 @@ function Header(props) {
   const { title, children } = props
 
   return (
-    <div id="header">
-      <div id="header-main" className="navbar navbar-default navbar-fixed-top">
+    <div id="header" style={{ height: '100px', width: '100%' }} >
+      <div id="header-main" style={{ height: '100px', width: '100%' }} className="navbar navbar-default navbar-fixed-top">
         <div className="container">
           <div id="header-main-row" className="row">
-            <div className="col-sm-3 col-md-2 col-lg-2 header-main-left">
+            <div className="col header-main-left">
               <div id="logo">
                 <a href="https://www2.gov.bc.ca">
                   <img src={bclogo} alt="B.C. Government Logo" />
@@ -53,20 +53,20 @@ function Header(props) {
                 <img src={mobileOpen} alt="Open Menu" />
               </button>
             </div>
-            <div className="col-sm-8 col-md-8 col-lg-6 hidden-xs">
+            <div className="col hidden-xs">
               <div className="bcgov-title">
                 <h1>
                   {title}
                 </h1>
               </div>
             </div>
-            <div id="navbar" className="collapse navbar-collapse">
-              <ul className="nav navbar-nav">
+            <nav id="navbar" className="collapse navbar navbar-collapse">
+              <ul className="mr-auto navbar-nav">
                 {children && children.map((ele, idx) => (
                   <li key={idx}>{ele}</li>
                 ))}
               </ul>
-            </div>
+            </nav>
           </div>
         </div>
         <div className="navigationRibbon">
